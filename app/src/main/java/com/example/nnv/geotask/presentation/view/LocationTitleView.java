6fb26f54a2ee1301b4a998886bf748a5097836de
Nikita Nemtin <blackeeh@gmail.com>
2017-04-26 09:24:29 +0400

    search_loc: LocationTitleView interface

--- app/src/main/java/com/example/nnv/geotask/presentation/view/LocationTitleView.java
+++ app/src/main/java/com/example/nnv/geotask/presentation/view/LocationTitleView.java
@@ -1,8 +1,19 @@
 package com.example.nnv.geotask.presentation.view;
 
+import android.location.Address;
+
+import com.arellomobile.mvp.MvpView;
+import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy;
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType;
+
+import java.util.List;
+
 /**
  * Created by nnv on 25.04.17.
  */
 
-public interface LocationTitleView {
+@StateStrategyType(AddToEndSingleStrategy.class)
+public interface LocationTitleView  extends MvpView {
+    void updateLocationList(List<Address> addressList);
+    void showError(String error);
 }
